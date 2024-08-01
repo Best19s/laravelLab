41001b10f7495a968e5df4fc2934a6447c3d316a
Best <chaiyawat.sonachai@gmail.com>
2024-08-02 01:40:45 +0700

    Create Project

---
+++ app/Http/Controllers/FirstController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FirstController extends Controller
+{
+    public function index() {
+        return "Hello, from FirstController.";
+    }
+
+    public function getParams($number) {
+        return "Hello, we got $number";
+    }
+
+    public function showLayout() {
+        return view("child");
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FirstController;
+
+Route::get('toController', [FirstController::class, "index"])->name("first.idx");
+Route::get('layout', [FirstController::class, "showLayout"]);
+Route::get('toController/{number}', [FirstController::class, "getParams"]);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('FirstPage', function () {
+    return view('FirstController');
+});
+
+// Route::get('Params', function (){
+//     return view('FirstController');
+// });
+
